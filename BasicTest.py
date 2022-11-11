1eb9d4ccf4deb56d01a34dde1b1fbcd1eff9d7ad
Cameron Thorp <cameron.thorp123@outlook.com>
2022-11-11 11:30:19 -0700

    Sal's Shipping problem

--- BasicTest.py
+++ BasicTest.py
@@ -28,3 +28,42 @@ elif (random_number == 9):
     answer = "Very doubtful."
 
 print(name + " asks: " + question + "\nMagic 8-Ball's answer: " + answer)
+
+#Sal's Shipping project
+weight = float(input("Enter weight of package in lbs. "))
+cost_ground = 0
+
+if weight <= 2:
+    cost_ground = 20 + (1.5 * weight)
+elif weight <= 6:
+    cost_ground = 20 + (3 * weight)
+elif weight <= 10:
+    cost_ground = 20 + (4 * weight)
+elif weight > 10:
+    cost_ground = 20 + (4.75 * weight)
+
+cost_drone = 0
+if weight <= 2:
+    cost_drone = (4.5 * weight)
+elif weight <= 6:
+    cost_drone = (9 * weight)
+elif weight <= 10:
+    cost_drone = (12 * weight)
+elif weight > 10:
+    cost_drone = (14.25 * weight)
+
+recommended_shipping = ""
+shipping_price = 0
+
+if (cost_ground >= 125) and (cost_drone >= 125):
+    recommended_shipping = "Ground Shippint Premium"
+    shipping_price = 125
+elif (cost_ground > cost_drone):
+    recommended_shipping = "Drone Shipping"
+    shipping_price = cost_drone
+elif (cost_ground <= cost_drone):
+    recommended_shipping = "Ground Shipping"
+    shipping_price = cost_ground
+
+print("Ground: $" + str(cost_ground) + "\nDrone: $" + str(cost_drone) + "\nPremium Ground: $125")
+print("Recommended shipping method: " + recommended_shipping + " - $" + str(shipping_price))
